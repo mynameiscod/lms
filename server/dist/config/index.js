46780ba922e5314677a77ca40a550c1b67a01dac
Developer <dev@lms.local>
2026-04-27 01:06:31 +0530

    chore: untrack server/dist build artifacts (already in .gitignore)

--- server/dist/config/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = void 0;
-var database_1 = require("./database");
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return database_1.connectDB; } });
-//# sourceMappingURL=index.js.map
